2015e5d57886f2aac7140ab1a7a6ed8fe561eb25
Bastiaan van Graafeiland <bastiaan89@live.nl>
2022-09-26 23:27:03 +0200

    exercise 4 - single line tags

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -50,8 +50,9 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: minmax(auto, max-content);
   gap: 8px;
 `;
 
@@ -61,6 +62,9 @@ const Tag = styled.li`
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
 export default PhotoGridItem;
